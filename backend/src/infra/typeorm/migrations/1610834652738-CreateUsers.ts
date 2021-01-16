f0948002a9602d2e222ad4902289039718d51c21
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-16 20:03:24 -0300

    feat: add users migration

---
+++ backend/src/infra/typeorm/migrations/1610834652738-CreateUsers.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, Table, QueryRunner } from 'typeorm';
+
+export default class CreateUsers1610834652738 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
