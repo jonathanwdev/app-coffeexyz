71986d15c806ac9c74555f3dc9ee19a98225d076
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-16 17:41:35 -0300

    feat: add a new method to repository

--- backend/src/repositories/IUserRepository.ts
+++ backend/src/repositories/IUserRepository.ts
@@ -1,6 +1,7 @@
 import User from '../infra/typeorm/entities/User';
 
 export default interface IUserRepository {
+  findByEmail(email: string): Promise<User | undefined>;
   create(email: string): Promise<User>;
   save(user: User): Promise<User>;
 }
