2b58ab2b2b9f8ff36e521233ceaea6d3a7fbe356
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-16 18:50:05 -0300

    feat: add routes and error handler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,27 @@
 import 'dotenv/config';
 import 'reflect-metadata';
-import express from 'express';
-
+import express, { Request, Response, NextFunction } from 'express';
+import ErrorHandler from './Errors/ErrorHandler';
+import routes from './infra/http/routes';
 import './infra/typeorm';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.send('Working');
+app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof ErrorHandler) {
+    return response.status(err.statuscode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+  console.error(err);
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
 });
 
 app.listen(process.env.APP_PORT, () =>
