db858265b951f4229e78558bde66d3ba89e6718b
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-16 18:13:56 -0300

    feat: add error hander

---
+++ backend/src/Errors/ErrorHandler.ts
@@ -0,0 +1,12 @@
+class ErrorHandler {
+  public readonly message: string;
+
+  public readonly statuscode: number;
+
+  constructor(message: string, statuscode = 400) {
+    this.message = message;
+    this.statuscode = statuscode;
+  }
+}
+
+export default ErrorHandler;
