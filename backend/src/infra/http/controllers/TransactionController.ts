c317b115db4ca541d6b0904992fcd06b3de6072c
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-17 17:50:26 -0300

    feat: add index method to transaction controller

--- backend/src/infra/http/controllers/TransactionController.ts
+++ backend/src/infra/http/controllers/TransactionController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import CreateTransactionService from '../../../services/CreateTransactionService';
+import ListTransactionService from '../../../services/ListTransactionService';
 import TransactionRepository from '../../typeorm/repositories/TransactionRepository';
 import TransactionItemsRepository from '../../typeorm/repositories/TransactionItemsRepository';
 
@@ -7,12 +8,21 @@ class TransactionController {
   public async create(req: Request, res: Response): Promise<Response> {
     const transactionRepository = new TransactionRepository();
     const transactionItemsRepository = new TransactionItemsRepository();
-    const productsService = new CreateTransactionService(
+    const transactionService = new CreateTransactionService(
       transactionRepository,
       transactionItemsRepository,
     );
-    const products = await productsService.execute(req.body, req.user.id);
+    const products = await transactionService.execute(req.body, req.user.id);
 
+    return res.json(products);
+  }
+
+  public async index(req: Request, res: Response): Promise<Response> {
+    const transactionRepository = new TransactionRepository();
+    const listTransactionService = new ListTransactionService(
+      transactionRepository,
+    );
+    const products = await listTransactionService.execute(req.user.id);
     return res.json(products);
   }
 }
