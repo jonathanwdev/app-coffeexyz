7056017588f541b18cc9c55e563f717204513fc5
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-17 15:46:41 -0300

    feat: add tupeorm transaction  repository

---
+++ backend/src/infra/typeorm/repositories/TransactionRepository.ts
@@ -0,0 +1,35 @@
+import { getRepository, Repository } from 'typeorm';
+import {
+  ITransactionRepository,
+  TransactionListData,
+} from '../../../repositories/ITransactionRepository';
+
+import Transaction from '../entities/Transaction';
+
+class TransactionRepository implements ITransactionRepository {
+  private ormRepository: Repository<Transaction>;
+
+  constructor() {
+    this.ormRepository = getRepository(Transaction);
+  }
+
+  public async findAllByUserId(
+    userId: string,
+  ): Promise<Transaction[] | undefined> {
+    const transactions = await this.ormRepository.find({
+      where: { id: userId },
+    });
+    return transactions;
+  }
+
+  public async create(data: TransactionListData): Promise<Transaction> {
+    const transaction = this.ormRepository.create(data);
+    return transaction;
+  }
+
+  public async save(transaction: Transaction): Promise<Transaction> {
+    return this.ormRepository.save(transaction);
+  }
+}
+
+export default TransactionRepository;
