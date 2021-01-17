59303450a1bb0a44ef72098bba27baee1b45e0ff
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-17 09:45:07 -0300

    feat: add product route and ensure that user is authenticated

--- backend/src/infra/http/routes/index.ts
+++ backend/src/infra/http/routes/index.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import AuthMiddleware from '../middleware/authMiddleware';
 import AuthController from '../controllers/AuthController';
+import ProductController from '../controllers/ProductController';
 
 const routes = Router();
 
 const authController = new AuthController();
+const productController = new ProductController();
 
-routes.post('/auth', authController.auth);
+routes.post('/auth', authController.create);
+
+routes.use(AuthMiddleware);
+routes.get('/products', productController.index);
 
 export default routes;
