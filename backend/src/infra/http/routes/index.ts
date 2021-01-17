31d3d21bbc3bf5210aa4068f7a014656f1306975
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-17 16:21:28 -0300

    feat: add transaction route

--- backend/src/infra/http/routes/index.ts
+++ backend/src/infra/http/routes/index.ts
@@ -2,15 +2,18 @@ import { Router } from 'express';
 import AuthMiddleware from '../middleware/authMiddleware';
 import AuthController from '../controllers/AuthController';
 import ProductController from '../controllers/ProductController';
+import TransactionController from '../controllers/TransactionController';
 
 const routes = Router();
 
 const authController = new AuthController();
 const productController = new ProductController();
+const transactionController = new TransactionController();
 
 routes.post('/auth', authController.create);
 
 routes.use(AuthMiddleware);
 routes.get('/products', productController.index);
+routes.post('/transaction', transactionController.create);
 
 export default routes;
