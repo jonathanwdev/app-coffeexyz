8ca6a7b7d36f119ec9f0d7c24b159e6ba02a2891
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-17 15:44:26 -0300

    feat: add transaction typeorm model

---
+++ backend/src/infra/typeorm/entities/Transaction.ts
@@ -0,0 +1,57 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('transactions')
+class Transaction {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  user_id: string;
+
+  @Column()
+  user_name: string;
+
+  @Column()
+  user_email: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  country: string;
+
+  @Column()
+  state: string;
+
+  @Column()
+  cep_number: number;
+
+  @Column()
+  card_type: string;
+
+  @Column()
+  card_name: string;
+
+  @Column()
+  card_number: number;
+
+  @Column()
+  card_expiresIn: string;
+
+  @Column()
+  card_cvv: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Transaction;
