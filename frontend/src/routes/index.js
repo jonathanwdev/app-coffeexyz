a9c2d7c51f802c7068fda78fe978530046b78edf
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-17 18:50:54 -0300

    feat: add routes switch creator

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,12 @@
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+
+import Login from '../Pages/Login';
+
+const Routes = () => (
+  <Switch>
+    <Route exact component={Login} />
+  </Switch>
+);
+
+export default Routes;
