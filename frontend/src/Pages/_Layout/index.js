b71ee227ec01b426c52418f96a5983e072c467a8
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-18 20:03:57 -0300

    feat: add cart to global layout

--- frontend/src/Pages/_Layout/index.js
+++ frontend/src/Pages/_Layout/index.js
@@ -3,12 +3,17 @@ import PropTypes from 'prop-types';
 import Header from '../../components/Header';
 import { useAuth } from '../../hooks/AuthContext';
 import { Container } from './styles';
+import Cart from '../../components/Cart';
+import { useCart } from '../../hooks/CartContext';
 
 const Layout = ({ children }) => {
   const { user } = useAuth();
+  const { visible } = useCart();
   return (
     <Container>
       {!!user && <Header />}
+      <Cart visible={visible} />
+
       {children}
     </Container>
   );
