e39cf12731ee18610383d8e4c7badda24ccc9e73
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-18 20:05:26 -0300

    feat: create add func and totoal sum to useCart

--- frontend/src/hooks/CartContext.js
+++ frontend/src/hooks/CartContext.js
@@ -1,19 +1,46 @@
-import React, { createContext, useCallback, useContext, useState } from 'react';
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import PropTypes from 'prop-types';
-import Cart from '../components/Cart';
+import { Format } from '../utils/formatProce';
 
 const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
   const [visible, setVisible] = useState(false);
+  const [cartProducts, setCartProducts] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
 
   const toggleCart = useCallback(() => {
     setVisible((state) => !state);
   }, []);
 
+  const addProductToCart = useCallback((product) => {
+    setCartProducts((lastCart) => {
+      if (!lastCart.find((item) => item.id === product.id)) {
+        return [...lastCart, product];
+      }
+      return [...lastCart];
+    });
+    setVisible(true);
+  }, []);
+
+  useEffect(() => {
+    setCartTotal(
+      Format(
+        cartProducts.reduce((totalSum, product) => totalSum + product.price, 0)
+      )
+    );
+  }, [cartProducts]);
+
   return (
-    <CartContext.Provider value={{ visible, toggleCart }}>
-      <Cart visible={visible} />
+    <CartContext.Provider
+      value={{ cartTotal, visible, toggleCart, cartProducts, addProductToCart }}
+    >
       {children}
     </CartContext.Provider>
   );
