78a3f131e8ce57eb32ac28cbfd797de477eee8ed
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-17 22:41:15 -0300

    feat: add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import ErrorHandler from './Errors/ErrorHandler';
 import routes from './infra/http/routes';
@@ -8,6 +9,7 @@ import './infra/typeorm';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
