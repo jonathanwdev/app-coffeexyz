d5306f18832989c6f77f984e71493a369861563f
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-18 16:52:30 -0300

    refactor: change product image

--- backend/src/infra/typeorm/migrations/script.ts
+++ backend/src/infra/typeorm/migrations/script.ts
@@ -5,7 +5,7 @@ INSERT INTO products(id, name, picture_url, price) VALUES
   ('3', 'café com leite', 'https://www.altoastral.com.br/wp-content/uploads/2016/11/cafe-com-leite-750x500.jpg', 4.5),
   ('4', 'suco de uva', 'https://www.vinicolacampestre.com.br/blog/wp-content/uploads/sites/2/2019/04/269395-suco-de-uva-e-vinho-quais-as-diferencas-para-a-saude-1.jpg', 5),
   ('5', 'suco de larnja', 'https://s2.glbimg.com/nD7Ao3HBAyAK7_g_Tt75yk1pEiY=/780x440/e.glbimg.com/og/ed/f/original/2019/01/15/31617293018_896bf29d55_k.jpg', 5),
-  ('6', 'coca cola', 'https://www.imigrantesbebidas.com.br/bebida/images/products/full/1988_Refrigerante_Coca_Cola_Latinha_220_ml.jpg', 4.5),
+  ('6', 'coca cola', 'https://f.i.uol.com.br/fotografia/2018/08/21/15348230475b7b8a8778a2e_1534823047_3x2_rt.jpg', 4.5),
   ('7', 'guaraná antarctica', 'https://cdn.awsli.com.br/600x450/1336/1336703/produto/53864371/89b022c41e.jpg', 4.5),
   ('8', 'sanduíche de presunto','https://t1.rg.ltmcdn.com/pt/images/6/7/1/img_sanduiche_clube_2176_600.jpg', 8.7),
   ('9', 'sanduíche de queijo','https://img.estadao.com.br/fotos/crop/600x400/resources/jpg/7/3/1543522402137.jpg',8),
