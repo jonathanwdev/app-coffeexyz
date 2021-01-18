9d4b9ce5c1ce6433ced895cff96902217d862cc1
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-17 22:45:00 -0300

    feat: create home route

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,10 +3,12 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import Login from '../Pages/Login';
+import Home from '../Pages/Home';
 
 const Routes = () => (
   <Switch>
-    <Route exact component={Login} />
+    <Route exact component={Login} path="/" />
+    <Route isPrivate component={Home} path="/home" />
   </Switch>
 );
 
