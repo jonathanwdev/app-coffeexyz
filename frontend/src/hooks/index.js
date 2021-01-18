3343e3bf0737dc4ed8a47bdda37b0ee487508838
Jonathan Souza <jonathan.wdev@gmail.com>
2021-01-17 22:43:39 -0300

    feat: add app provider import handler

---
+++ frontend/src/hooks/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { AuthProvider } from './AuthContext';
+
+const AppProvider = ({ children }) => <AuthProvider>{children}</AuthProvider>;
+
+export default AppProvider;
+
+AppProvider.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
